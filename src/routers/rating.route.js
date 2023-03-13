44bfe931597b5f43c91c363a4b50c19dd884b0c9
anhcaols <caotheanhls2001@gmail.com>
2023-03-13 16:05:00 +0700

    fix middleware rating

--- src/routers/rating.route.js
+++ src/routers/rating.route.js
@@ -2,8 +2,8 @@ import 'express-async-errors';
 
 import { Router } from 'express';
 import { createRatingController } from '../controllers/rating.controller.js';
-import { verifyTokenMiddleware } from '../middlewares/verify-token.middleware.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 export const ratingRouter = Router();
 
-ratingRouter.post('/ratings', verifyTokenMiddleware(), createRatingController);
+ratingRouter.post('/ratings', authMiddleware(), createRatingController);
